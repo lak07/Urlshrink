e1628a0896cf8b70ffb193a4bde70e31d52d88c3
Lakshay Sharma <lavi9sar@gmail.com>
2017-04-17 20:28:48 +0530

    Update settings.py

--- UrlShortener/settings.py
+++ UrlShortener/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = ‘/static/’
-STATIC_ROOT = ‘staticfiles’
+STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, ‘static’),
+    os.path.join(BASE_DIR, 'static'),
 )
 
-SECURE_PROXY_SSL_HEADER = (‘HTTP_X_FORWARDED_PROTO’, ‘https’)
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
