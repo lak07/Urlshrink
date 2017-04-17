5b1300a861e4ade1677129043aa114a2aba12098
Lakshay Sharma <lavi9sar@gmail.com>
2017-04-17 23:26:18 +0530

    added url validation

--- f22/views.py
+++ f22/views.py
@@ -16,7 +16,7 @@ def post_new(request):
     form = PostForm() 
     link=ShortenURL()
     response=request.GET.get('Url')
-    val = URLValidator(verify_exists=False
+    val = URLValidator(verify_exists=False)
     try:
         val('http://www.google.com')
     except ValidationError, e:
