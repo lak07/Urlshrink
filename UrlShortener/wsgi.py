532ae722e49c357b8ea440628520f5674df2e3ac
Lakshay Sharma <lavi9sar@gmail.com>
2017-04-17 22:51:59 +0530

    Update wsgi.py

--- UrlShortener/wsgi.py
+++ UrlShortener/wsgi.py
@@ -1,15 +1,10 @@
-"""
-WSGI config for UrlShortener project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
 import os
+import sys
+
+
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'UrlShortener.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-from whitenoise.django import DjangoWhiteNoise
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "UrlShortener.settings")
-application = DjangoWhiteNoise(application)
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = get_wsgi_application()
