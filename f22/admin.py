b470c75e388554612bfff5198c532b7e6eaa90b3
Lakshay Sharma <lavi9sar@gmail.com>
2017-04-17 19:51:43 +0530

    Added sqlite database

--- f22/admin.py
+++ f22/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from f22.models import Post
 # Register your models here.
+ 
+class UrlsAdmin(admin.ModelAdmin):
+    list_display = ('short_id','httpurl')
+ 
+admin.site.register(Post, UrlsAdmin) # Register your models here.
